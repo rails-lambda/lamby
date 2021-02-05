6bcd4cb7533e9389199da7222836f06790690eae
Morten Trolle <mtrolle@gmail.com>
2021-02-05 21:47:39 +0100

    Removing dotenv requirement
    Gemspec is not defining dotenv dependency and I don't find any other reference to it, so requiring it fails right now.

--- lib/lamby/templates/alb/app.rb
+++ lib/lamby/templates/alb/app.rb
@@ -1,6 +1,5 @@
 ENV['RAILS_SERVE_STATIC_FILES'] = '1'
 require_relative 'config/boot'
-require 'dotenv' ; Dotenv.load ".env.#{ENV['RAILS_ENV']}"
 require 'lamby'
 require_relative 'config/application'
 require_relative 'config/environment'
