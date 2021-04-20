ed25d6c39ae711cb1bb6f7915b480bbe42de454f
Ken Collins <ken@metaskills.net>
2021-04-20 08:24:15 -0400

    Update Rack Deflate Tests.

--- test/rack_deflate_test.rb
+++ test/rack_deflate_test.rb
@@ -5,24 +5,65 @@ class RackDeflateTest < LambySpec
   let(:app)     { Rack::Builder.new { run Rails.application }.to_app }
   let(:context) { TestHelpers::LambdaContext.new }
 
-  describe 'Using Rack Deflate Middleware' do
-
-    it 'get' do
-      event = TestHelpers::Events::HttpV2.create
-      result = Lamby.handler app, event, context, rack: :http
-      expect(result[:statusCode]).must_equal 200
-    end
-
-    it 'get - test redirect' do
-      event = TestHelpers::Events::HttpV2.create(
-        'rawPath' => '/production/redirect_test',
-        'requestContext' => { 'http' => {'path' => '/production/redirect_test'} }
-      )
-      result = Lamby.handler app, event, context, rack: :http
-      expect(result[:statusCode]).must_equal 301
-      refute result[:headers]['Location'].nil?
-    end
-    
+  it 'get - Rest' do
+    event = TestHelpers::Events::Rest.create
+    result = Lamby.handler app, event, context, rack: :rest
+    expect(result[:statusCode]).must_equal 200
   end
-  
+
+  it 'get - Rest with redirect' do
+    event = TestHelpers::Events::Rest.create(
+      'path' => '/redirect_test',
+      'requestContext' => { 'path' => '/redirect_test'}
+    )
+    result = Lamby.handler app, event, context, rack: :rest
+    expect(result[:statusCode]).must_equal 301
+    refute result[:headers]['Location'].nil?
+  end
+
+  it 'get - HttpV1' do
+    event = TestHelpers::Events::HttpV1.create
+    result = Lamby.handler app, event, context, rack: :http
+    expect(result[:statusCode]).must_equal 200
+  end
+
+  it 'get - HttpV1 with redirect' do
+    event = TestHelpers::Events::HttpV1.create(
+      'path' => '/production/redirect_test',
+      'requestContext' => { 'path' => '/production/redirect_test'}
+    )
+    result = Lamby.handler app, event, context, rack: :http
+    expect(result[:statusCode]).must_equal 301
+    refute result[:headers]['Location'].nil?
+  end
+
+  it 'get - HttpV2' do
+    event = TestHelpers::Events::HttpV2.create
+    result = Lamby.handler app, event, context, rack: :http
+    expect(result[:statusCode]).must_equal 200
+  end
+
+  it 'get - HttpV2 with redirect' do
+    event = TestHelpers::Events::HttpV2.create(
+      'rawPath' => '/production/redirect_test',
+      'requestContext' => { 'http' => {'path' => '/production/redirect_test'} }
+    )
+    result = Lamby.handler app, event, context, rack: :http
+    expect(result[:statusCode]).must_equal 301
+    refute result[:headers]['Location'].nil?
+  end
+
+  it 'get - Alb' do
+    event = TestHelpers::Events::Alb.create
+    result = Lamby.handler app, event, context, rack: :alb
+    expect(result[:statusCode]).must_equal 200
+  end
+
+  it 'get - Alb with redirect' do
+    event = TestHelpers::Events::Alb.create 'path' => '/redirect_test'
+    result = Lamby.handler app, event, context, rack: :alb
+    expect(result[:statusCode]).must_equal 301
+    refute result[:headers]['Location'].nil?
+  end
+
 end
