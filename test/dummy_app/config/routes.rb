0db0d257f48889ccb5e4324f33f261ed3b7844ff
Ken Collins <ken@metaskills.net>
2023-07-07 16:07:25 -0400

    Safely pass percent symbols in paths.

--- test/dummy_app/config/routes.rb
+++ test/dummy_app/config/routes.rb
@@ -4,6 +4,7 @@ Dummy::Application.routes.draw do
   post 'login', to: 'application#login'
   delete 'logout', to: 'application#logout'
   get 'exception', to: 'application#exception'
+  get 'percent/*path', to: 'application#percent'
   get 'cooks', to: 'application#cooks'
   get 'redirect_test', to: redirect('/')
 end
