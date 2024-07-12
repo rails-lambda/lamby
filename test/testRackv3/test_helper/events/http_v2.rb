acb84a6c6e5b0d50cba550112c6eedf5dfa934e5
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2024-07-12 11:43:41 -0400

    Lowercase headers and rackv3 no longer includes a body on redirects

--- test/testRackv3/test_helper/events/http_v2.rb
+++ test/testRackv3/test_helper/events/http_v2.rb
@@ -4,7 +4,44 @@ module TestHelpers
 
       # Via Custom Domain Name integration.
       #
-      self.event = {"version"=>"2.0", "routeKey"=>"$default", "rawPath"=>"/production/", "rawQueryString"=>"colors[]=blue&colors[]=red", "cookies"=>["signal1=test", "signal2=control"], "headers"=>{"accept"=>"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "accept-encoding"=>"gzip, deflate, br", "accept-language"=>"en-us", "content-length"=>"0", "host"=>"myawesomelambda.example.com", "user-agent"=>"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Safari/605.1.15", "x-amzn-trace-id"=>"Root=1-5e7fe714-fee6909429159440eb352c40", "x-forwarded-for"=>"72.218.219.201", "x-forwarded-port"=>"443", "x-forwarded-proto"=>"https"}, "requestContext"=>{"accountId"=>nil, "apiId"=>"n12pmpajak", "domainName"=>"myawesomelambda.example.com", "domainPrefix"=>"myawesomelambda", "http"=>{"method"=>"GET", "path"=>"/production/", "protocol"=>"HTTP/1.1", "sourceIp"=>"72.218.219.201", "userAgent"=>"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Safari/605.1.15"}, "requestId"=>"KSP7Mj94IAMEMFQ=", "routeKey"=>"$default", "stage"=>"production", "time"=>"01/Apr/2020:02:18:09 +0000", "timeEpoch"=>1585707489142}, "isBase64Encoded"=>false}.freeze
+      self.event = {
+        "version" => "2.0",
+        "routeKey" => "$default",
+        "rawPath" => "/production/",
+        "rawQueryString" => "colors[]=blue&colors[]=red",
+        "cookies" => ["signal1=test", "signal2=control"],
+        "headers" => {
+          "accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+          "accept-encoding" => "gzip, deflate, br",
+          "accept-language" => "en-us",
+          "content-length" => "0",
+          "host" => "myawesomelambda.example.com",
+          "user-agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Safari/605.1.15",
+          "x-amzn-trace-id" => "Root=1-5e7fe714-fee6909429159440eb352c40",
+          "x-forwarded-for" => "72.218.219.201",
+          "x-forwarded-port" => "443",
+          "x-forwarded-proto" => "https"
+        },
+        "requestContext" => {
+          "accountId" => nil,
+          "apiId" => "n12pmpajak",
+          "domainName" => "myawesomelambda.example.com",
+          "domainPrefix" => "myawesomelambda",
+          "http" => {
+            "method" => "GET",
+            "path" => "/production/",
+            "protocol" => "HTTP/1.1",
+            "sourceIp" => "72.218.219.201",
+            "userAgent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Safari/605.1.15"
+          },
+          "requestId" => "KSP7Mj94IAMEMFQ=",
+          "routeKey" => "$default",
+          "stage" => "production",
+          "time" => "01/Apr/2020:02:18:09 +0000",
+          "timeEpoch" => 1585707489142
+        },
+        "isBase64Encoded" => false
+      }.freeze
 
       # Via CloudFront directly to API Gateway w/Origin Path.
       #
